14387c0d74708962f94c73507cbafa4a3badde05
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 18:21:16 +0100

    träning om string method med typeof och hur kan det vara number och hur kan det vara string

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* global console, alert , prompt*/
 
-
-
+//  typeof
+// length
 
 /*
 var myString = " I Love JavaScript";
@@ -35,7 +35,7 @@ console.log(myString);
 
 console.log(typeof(myString));
 */
-
+/*
 var myString = 'I Love JavaScript';
 
 console.log(myString);
@@ -44,5 +44,36 @@ console.log(typeof(myString));
 
 console.log(myString.length);
 
+var myNumber = 100;
+
+console.log(myNumber);
+
+console.log(typeof(myNumber));
+*/
 
 
+/*
+var myNumber = 100;
+
+console.log(myNumber);
+
+console.log(typeof (myNumber));
+
+var myNewNumber = myNumber.toString();
+
+console.log(myNewNumber);
+
+console.log(typeof(myNewNumber));
+*/
+
+var myNumber = 100;
+
+console.log(myNumber);
+
+console.log(typeof (myNumber));
+
+var myNewNumber = String(myNumber);
+
+console.log(myNewNumber);
+
+console.log(typeof(myNewNumber));
